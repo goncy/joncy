7ba361cdc6efe91e2c978babb7d323f74e929043
goncy <gonzalo.pozzo4@gmail.com>
2021-07-28 17:23:12 -0300

    Add lazy load animation for images

--- ui/display/FixedImage.tsx
+++ ui/display/FixedImage.tsx
@@ -1,12 +1,37 @@
-import Image from "next/image";
-import {chakra} from "@chakra-ui/react";
+import * as React from "react";
+import Image, {ImageProps as NextImageProps} from "next/image";
+import {chakra, ImageProps as ChakraImageProps} from "@chakra-ui/react";
 
-const FixedImage = chakra(Image, {
+type Props = ChakraImageProps & NextImageProps;
+
+const Factory = chakra(Image, {
   baseStyle: {
     backgroundColor: "white",
   },
   shouldForwardProp: (prop) =>
-    ["src", "width", "height", "layout", "placeholder", "blurDataURL", "alt"].includes(prop),
+    [
+      "src",
+      "width",
+      "height",
+      "layout",
+      "blurDataURL",
+      "alt",
+      "onLoadingComplete",
+      "placeholder",
+    ].includes(prop),
 });
+
+function FixedImage(props: Props): JSX.Element {
+  const [isLoaded, toggleLoaded] = React.useState(false);
+
+  return (
+    <Factory
+      {...props}
+      filter={isLoaded ? "grayscale(0) blur(0)" : "grayscale(1) blur(12px)"}
+      transition="filter .5s"
+      onLoadingComplete={() => toggleLoaded(true)}
+    />
+  );
+}
 
 export default FixedImage;
