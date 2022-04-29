7bffb1676c78c144c4a4ca151e3de77d10586013
goncy <gonzalo.pozzo4@gmail.com>
2022-04-29 13:33:51 -0300

    Remove workbox files

--- scripts/parse-southteams-positions.mjs
+++ scripts/parse-southteams-positions.mjs
@@ -4,20 +4,7 @@ import {fileURLToPath} from "url";
 
 import papaparse from "papaparse";
 
-const POSITIONS = `769 - Sr. QA Automation (3 opening/s)
-We are looking for a QA Automation engineer with +3 years of experience building automation tests and unit tests using Javascript. Experience with Mocha, WebDriver, Cypress or similar frameworks is required. IMPORTANT: This position requires candidates from Argentina or Uruguay.
-
-Average Rate: $4000 - This rate is just average.
-
-775 - Sr. DevOps Engineer (Azure) (1 opening/s)
-We are looking for a DevOps Engineer with at least 4 years in the role. Experience with Microsoft Azure is required, experience with AWS is a plus. As well as Docker, Azure Kubernetes Service, Azure IaaS Compute / Storage / Databases / Networking/ Monitoring-auditing & Security. AzureDevOps: CI/CD Pipelines, Ability to set permission and maintain Projects. Scripting Language (Automation), PowerShell Knowledge of Agile Development, Development and Cloud Architecture.
-
-Average Rate: $5000 - This rate is just average.
-
-781 - Sr. Backend Node.js (4 opening/s)
-We are looking for a Sr. Backend Developer with at least 3 years of experience with Node.Js building rest APIs with Express.js or COA. Experience with integration with third-party systems is a must. Knowledge on other Javascript frameworks is a plus. IMPORTANT: This position requires candidates from Argentina or Uruguay.
-
-Average Rate: $4500 - This rate is just average.`;
+const POSITIONS = ``;
 
 // Parse positions
 const data = POSITIONS.split(` - This rate is just average.`)
