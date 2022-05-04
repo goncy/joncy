3e9bf6bf900a04de6820a0d6bc2f10b861f564b1
goncy <gonzalo.pozzo4@gmail.com>
2022-05-04 13:56:46 -0300

    Update cache headers

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -6,7 +6,7 @@ export default async function handler(_req: NextApiRequest, res: NextApiResponse
   const jobs = await api.list();
 
   res.setHeader("Content-Type", "application/xml");
-  res.setHeader("Cache-Control", "max-age=21600");
+  res.setHeader("Cache-Control", "public, s-max-age=21600, stale-while-revalidate=59");
 
   return res.end(`<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
