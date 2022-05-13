9842bb3391359aaae737057fea67ba8c07b38527
goncy <gonzalo.pozzo4@gmail.com>
2022-05-13 13:51:19 -0300

    Update image

--- ui/display/FixedImage.tsx
+++ ui/display/FixedImage.tsx
@@ -35,7 +35,9 @@ function FixedImage(props: Props): JSX.Element {
       <Factory
         {...props}
         filter={isLoading ? "grayscale(1) blur(12px)" : "grayscale(0) blur(0)"}
+        height={props.height}
         transition="filter .5s"
+        width={props.width}
         onLoadingComplete={() => toggleLoading(false)}
       />
     </Box>
